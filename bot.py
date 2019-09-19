929814169d60aee56c1b053a5ce080d8662703cd
Your Name <you@example.com>
2019-09-20 04:58:43 +0700

    g

--- bot.py
+++ bot.py
@@ -45,9 +45,9 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.source.user_id))
-    #line_bot_api.push_message(
-    #    event.user_id,
-    #    ImageSendMessage(original_content_url='https://hoodline.imgix.net/uploads/story/image/579057/..destination_photo_url..RIOA-sky.jpg.jpg', preview_image_url='https://hoodline.imgix.net/uploads/story/image/579057/..destination_photo_url..RIOA-sky.jpg.jpg'))
+    line_bot_api.push_message(
+        event.source.user_id,
+        ImageSendMessage(original_content_url='https://hoodline.imgix.net/uploads/story/image/579057/..destination_photo_url..RIOA-sky.jpg.jpg', preview_image_url='https://hoodline.imgix.net/uploads/story/image/579057/..destination_photo_url..RIOA-sky.jpg.jpg'))
 
 
 
