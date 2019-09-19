42571ee1e19aacfa4881b4e5cf2917517b2d1c43
Your Name <you@example.com>
2019-09-19 08:04:46 +0700

    s

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('L4nfx9rvGHa4wm09LHokrFpTvS9zD5G8DzlYd8R1hIFiUsCOdU8fyZC/TT5sKjcK+fs+7EE5WKqjKVyxFCCSgCeJope5wS07yuY86Rsmm/B69GasZJjU7EWu97xLEUW1VTeg+B3kW8+bbG63CiU2fAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('954e358c46fc39d8e061a44dd918de4f')
 
 @app.route("/")
 def hello():
