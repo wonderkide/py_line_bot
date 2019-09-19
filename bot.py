99930bd28d6306cd5451a7b3e6dd280e7dbde3fb
Your Name <you@example.com>
2019-09-19 11:00:52 +0700

    s

--- bot.py
+++ bot.py
@@ -29,14 +29,14 @@ def webhook():
 
     return 'OK'
     
-'''
+
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         ImageSendMessage(even.message.original_content_url, even.message.preview_image_url))
-'''
 
+'''
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     #user = line_bot_api.get_profile(event.user_id)
@@ -46,7 +46,7 @@ def handle_message(event):
     line_bot_api.push_message(
         event.user_id,
         ImageSendMessage(original_content_url='https://hoodline.imgix.net/uploads/story/image/579057/..destination_photo_url..RIOA-sky.jpg.jpg', preview_image_url='https://hoodline.imgix.net/uploads/story/image/579057/..destination_photo_url..RIOA-sky.jpg.jpg'))
-
+'''
 
 
 if __name__ == "__main__":
