43ff10140a0c6b585fde2680d0d14966ad370821
Your Name <you@example.com>
2019-09-20 16:21:04 +0700

    g

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ def handle_message(event, destination):
     elif event.message.type == 'image':
         #img = line_bot_api.get_message_content(event.message.id)
         if event.message.content_provider.type == 'line':
-            #message_content = line_bot_api.get_message_content(event.message.id)
+            message_content = line_bot_api.get_message_content(event.message.id)
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=event.message.content_provider.type))
